debc125532611a4e33ecb47bb94e4d380d9415b1
kemillymotta <kemillymotta31@gmail.com>
2026-03-28 21:50:38 -0300

    feat: add main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,73 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package application;
+
+import entities.ToDoManager;
+
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        ToDoManager manager = new ToDoManager();
+
+        int option = 0;
+        while (option != 6) {
+            System.out.println("==== TO DO LIST ====");
+            System.out.println("1 - Add task");
+            System.out.println("2 - List tasks");
+            System.out.println("3 - Update task");
+            System.out.println("4 - Delete task");
+            System.out.println("5 - Mark task as done");
+            System.out.println("6 - Exit\n");
+            System.out.print("\nOption: ");
+            option = sc.nextInt();
+
+            String title;
+            int id;
+            switch (option) {
+                case 1:
+                    System.out.print("Enter task title: ");
+                    sc.nextLine();
+                    title = sc.nextLine();
+                    manager.addTasks(title);
+                    break;
+                case 2:
+                    manager.listTasks();
+                    break;
+                case 3:
+                    System.out.print("Enter task id: ");
+                    id = sc.nextInt();
+                    System.out.print("Enter new task title: ");
+                    sc.nextLine();
+                    title = sc.nextLine();
+                    manager.updateTask(id, title);
+                    break;
+                case 4:
+                    System.out.print("Enter task id: ");
+                    id = sc.nextInt();
+                    manager.deleteTask(id);
+                    break;
+                case 5:
+                    System.out.print("Enter task id: ");
+                    id = sc.nextInt();
+                    manager.markAsDone(id);
+                    break;
+                case 6:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid option!");
+                    break;
+            }
         }
+
+
+        sc.close();
     }
+
 }
+
+
+
+
